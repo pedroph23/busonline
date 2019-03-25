8eaaf197852e9c4681f0deb01529d1dee6c1f740
Pedro Henrique Guimaraes Ramalho <pedro.ramalho@basis.com.br>
2019-03-25 11:28:10 -0300

    Revert "ALteracoes"
    This reverts commit d9f44acb300df0a81954e6d49925ef4c2bc8f49e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppService } from './app.service';
 import { AgmCoreModule } from '@agm/core';
 
 import 'rxjs';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   imports:      
@@ -19,8 +17,7 @@ import { environment } from '../environments/environment';
   HttpClientModule,
   AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw'
-    }),
-  ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    })
   ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
